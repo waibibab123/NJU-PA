6154ddf48c83ef5f2662e8148182c42e82b8ded8
tracer-ics2023 <tracer@njuics.org>
2024-03-30 18:25:39 +0800

    >  compile NEMU 8208211225 董名喆 Linux dmz-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:25:39 up  1:30,  1 user,  load average: 0.23, 0.17, 0.17

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -54,6 +54,7 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
+static int cmd_si(char *args);
 
 static struct {
   const char *name;
@@ -63,6 +64,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si","si [n]: run n steps,n is optional,if not given,run 1 step", cmd_si},
 
   /* TODO: Add more commands */
 
@@ -90,6 +92,16 @@ static int cmd_help(char *args) {
     }
     printf("Unknown command '%s'\n", arg);
   }
+  return 0;
+}
+
+static int cmd_si(char *args){
+  int step;
+  if(args == NULL)
+  step = 1;
+  else 
+  sscanf(args,"%d",&step);
+  cpu_exec(step);
   return 0;
 }
 
