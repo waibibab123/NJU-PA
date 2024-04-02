68c3fa176516a890af69de0ecd4bba8d24743f4e
tracer-ics2023 <tracer@njuics.org>
2024-04-02 13:18:24 +0800

    >  compile NEMU 8208211225 董名喆 Linux dmz-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:18:24 up  2:55,  1 user,  load average: 0.56, 0.46, 0.37

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -299,6 +299,7 @@ uint32_t eval(int p,int q){
       }
     }
     int op_type = tokens[op].type;
+    printf("%c",op_type);
     uint32_t val1 = eval(p, op - 1);
     uint32_t val2 = eval(op + 1, q);
     switch (op_type){
