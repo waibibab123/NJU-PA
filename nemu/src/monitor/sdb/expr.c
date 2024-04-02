f8a994f7dd734ab54c515fd17856b1f5c04d77b3
tracer-ics2023 <tracer@njuics.org>
2024-04-02 13:02:00 +0800

    >  compile NEMU 8208211225 董名喆 Linux dmz-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:02:00 up  2:39,  1 user,  load average: 0.28, 0.35, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -255,6 +255,7 @@ uint32_t eval(int p,int q){
     return atoi(tokens[p].str);
   }
   else if(check_parentheses(p, q) == true){
+    printf("fuckyou");
     return eval(p + 1, q - 1);
   }
   else{
